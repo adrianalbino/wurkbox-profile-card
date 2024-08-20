31d49d9e48868479da90ad9df7ebf1a00a553ad5
aalbino <21102558@usc.edu.ph>
2024-08-20 14:47:05 +0800

    chore: cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react"
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import "./index.css"
 import Navbar from "./components/Navbar"
 import Client from "./components/Client"
